665cb7e93a310a8f5de055c3fc2d06647619dd0d
MSC020\Matias <m@gmail.com>
2018-10-17 15:12:40 -0300

    Se agrego una lista de peliculas que se muestra en una tabla en la home

--- WebContent/WEB-INF/views/home.jsp
+++ WebContent/WEB-INF/views/home.jsp
@@ -11,14 +11,37 @@
 </head>
 <body>
 	<h1>Lista de peliculas</h1>
-	
-	
-	<o1>
-		<c:forEach items="${peliculas }" var ="pelicula">
-			<li> ${pelicula } </li> 
+
+
+	<o1> <c:forEach items="${peliculas }" var="pelicula">
+		<li>${pelicula }</li>
+	</c:forEach> </o1>
+	Tabla de películas
+
+	<table border="1">
+		<thread>
+			<tr>
+				<th>Id</th>
+				<th>Titulo</th>
+				<th>Duracion</th>
+				<th>Clasificacion</th>
+				<th>Genero</th>
 			
-		</c:forEach>
-	</o1>
+			</tr>
+		</thread>
+		<tbody>
+			<c:forEach items="${peliculas }" var="pelicula">
+			<tr>
+				<td>${pelicula.id }</td>
+				<td>${pelicula.titulo }</td>
+				<td>${pelicula.duracion } min.</td>
+				<td>${pelicula.clasificacion }</td>
+				<td>${pelicula.genero }</td>
+			</tr>
+			</c:forEach> </o1>
+
+		</tbody>
+	</table>
 
 </body>
 </html>
--- src/main/java/com/holamundo/app/controller/HomeController.java
+++ src/main/java/com/holamundo/app/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.holamundo.app.controller;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.holamundo.app.model.Pelicula;
 
 @Controller
 public class HomeController {
@@ -20,15 +24,13 @@ public class HomeController {
 	@RequestMapping(value="/", method=RequestMethod.GET)
 	public String mostrarPrincipal(Model model)
 	{
-		List<String> peliculas = new ArrayList<String>();
+		List<Pelicula> peliculas = getLista();
 		
-		peliculas.add("Rapido y Furioso");
-		peliculas.add("El aro");
-		peliculas.add("Alien");
+//		peliculas.add("Rapido y Furioso");
+//		peliculas.add("El aro");
+//		peliculas.add("Alien");
 		
 		model.addAttribute("peliculas", peliculas);
-		
-		
 		
 		return "home";
 	}
@@ -46,4 +48,50 @@ public class HomeController {
 		
 		return "detalle";
 	}
+	
+	private List<Pelicula> getLista()
+	{
+		SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
+		List<Pelicula> lista = null;
+		
+		try {
+		
+			lista = new ArrayList<Pelicula>();
+			Pelicula pelicula1 = new Pelicula();
+			pelicula1.setId(1);
+			pelicula1.setTitulo("Pelicula 1");
+			pelicula1.setDuracion(120);
+			pelicula1.setClasificacion("B");
+			pelicula1.setGenero("Aventura");
+			pelicula1.setFechaEstreno(formatter.parse("02-05-2017"));
+
+			Pelicula pelicula2 = new Pelicula();
+			pelicula2.setId(2);
+			pelicula2.setTitulo("Pelicula 2");
+			pelicula2.setDuracion(110);
+			pelicula2.setClasificacion("A");
+			pelicula2.setGenero("Infantil");
+			pelicula2.setFechaEstreno(formatter.parse("02-10-2017"));
+			
+			Pelicula pelicula3 = new Pelicula();
+			pelicula3.setId(3);
+			pelicula3.setTitulo("Pelicula 3");
+			pelicula3.setDuracion(150);
+			pelicula3.setClasificacion("C");
+			pelicula3.setGenero("Aventura");
+			pelicula3.setFechaEstreno(formatter.parse("06-08-2018"));
+			
+			lista.add(pelicula1);
+			lista.add(pelicula2);
+			lista.add(pelicula3);
+			
+			
+		} catch (ParseException e) {
+			System.out.println("Error:" + e.getMessage());
+			
+		}
+		
+		return lista;
+	}
+	
 }
